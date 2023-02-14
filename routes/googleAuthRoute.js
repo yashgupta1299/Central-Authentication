3612ce949641ecb186d4df552118ccf7f8e9d536
Yash Gupta <yashgupta1249@gmail.com>
2023-02-14 18:53:02 +0530

    update

--- routes/googleAuthRoute.js
+++ routes/googleAuthRoute.js
@@ -5,58 +5,52 @@ const passport = require('../controllers/googleAuthController');
 const router = express.Router();
 router.use(passport.initialize());
 
-router
-    .route('/sta')
-    .get(
-        '/',
-        passport.authenticate('google', {
-            session: false,
-            scope: ['profile', 'email']
-        })
-    )
-    .get(
-        '/callback',
-        passport.authenticate('google', {
-            failureRedirect: `${process.env.FRONTEND_DOMAIN}/?alert=authenticationFailed`,
-            session: false
-        }),
-        authController.googleGetShortTimeAccessToken
-    );
+router.get(
+    '/sta',
+    passport.authenticate('google', {
+        session: false,
+        scope: ['profile', 'email']
+    })
+);
+router.get(
+    '/sta/callback',
+    passport.authenticate('google', {
+        failureRedirect: `${process.env.FRONTEND_DOMAIN}/?alert=authenticationFailed`,
+        session: false
+    }),
+    authController.googleGetShortTimeAccessToken
+);
 
-router
-    .route('/forgotPassword')
-    .get(
-        '/',
-        passport.authenticate('google', {
-            session: false,
-            scope: ['profile', 'email']
-        })
-    )
-    .get(
-        '/callback',
-        passport.authenticate('google', {
-            failureRedirect: `${process.env.FRONTEND_DOMAIN}/?alert=authenticationFailed`,
-            session: false
-        }),
-        authController.googleGetShortTimeAccessToken
-    );
+router.get(
+    '/forgotPassword',
+    passport.authenticate('google', {
+        session: false,
+        scope: ['profile', 'email']
+    })
+);
+router.get(
+    '/forgotPassword/callback',
+    passport.authenticate('google', {
+        failureRedirect: `${process.env.FRONTEND_DOMAIN}/?alert=authenticationFailed`,
+        session: false
+    }),
+    authController.googleGetShortTimeAccessToken
+);
 
-router
-    .route('/signInwithGoogle')
-    .get(
-        '/',
-        passport.authenticate('google', {
-            session: false,
-            scope: ['profile', 'email']
-        })
-    )
-    .get(
-        '/callback',
-        passport.authenticate('google', {
-            failureRedirect: `${process.env.FRONTEND_DOMAIN}/?alert=authenticationFailed`,
-            session: false
-        }),
-        authController.googleGetShortTimeAccessToken
-    );
+router.get(
+    '/signInwithGoogle',
+    passport.authenticate('google', {
+        session: false,
+        scope: ['profile', 'email']
+    })
+);
+router.get(
+    '/signInwithGoogle/callback',
+    passport.authenticate('google', {
+        failureRedirect: `${process.env.FRONTEND_DOMAIN}/?alert=authenticationFailed`,
+        session: false
+    }),
+    authController.googleGetShortTimeAccessToken
+);
 
 module.exports = router;
